fb09c136be850b3b6cfe58fc667c6212e2066518
dictateur <joelpromero140@gmail.com>
2025-07-14 23:57:52 +0200

    Test Mockito Finnhub Service

--- src/test/java/com/example/FinnhubServiceTest.java
+++ src/test/java/com/example/FinnhubServiceTest.java
@@ -1,8 +1,11 @@
 package com.example;
 
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.mockito.*;
 
 public class FinnhubServiceTest {
@@ -14,10 +17,22 @@ public class FinnhubServiceTest {
     private FinnhubService service;
     
 
+    @SuppressWarnings("unchecked")
     @Test
-    public void testGetStockInfo() {
-        String symbol = "APPL";
-        
-        
+    public void testGetStockInfo() throws Exception  {
+        mockHttpClient = Mockito.mock(HttpClient.class);
+        HttpResponse<String> mockResponse = Mockito.mock(HttpResponse.class);
+
+        Mockito.when(mockResponse.body()).thenReturn("{\"c\": 150.0, \"d\": 2.0, \"dp\": 1.3}");
+        Mockito.when(mockHttpClient.send(Mockito.any(HttpRequest.class), Mockito.any(HttpResponse.BodyHandler.class)))
+           .thenReturn(mockResponse);
+
+        service = new FinnhubService("FAKE_APIKEY", mockHttpClient);
+
+        // Act
+        String result = service.getStockInfo("AAPL");
+
+        // Assert
+        Assertions.assertTrue(result.contains("\"c\": 150.0"));
     }
 }
